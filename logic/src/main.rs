861b10930e422cd33b027cadc6b44c5ade8289af
Aryan <malhotraaryan570@gmail.com>
2024-09-10 20:38:50 +0530

    server_initial_done_working_websocket

--- logic/src/main.rs
+++ logic/src/main.rs
@@ -1,3 +1,7 @@
+use async_std::task::block_on;
+use logic::server;
 fn main() {
-    println!("Hello, world!");
+    let connect = server("127.0.0.1:8000");
+    let _connect = block_on(connect);
+    // println!("Hello, world!");
 }

b6c749f0d738b43d28c5251551ea0ddb5d20617e
Aryan <malhotraaryan570@gmail.com>
2024-09-11 13:20:10 +0530

    connected_time_to_plan

--- pixels/src/app/components/Start.tsx
+++ pixels/src/app/components/Start.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import Player from './Player'
 const Start = () => {
+  useEffect(() => {
+    const socket = new WebSocket("ws://127.0.0.1:8000");
+    socket.onopen = () => {
+      socket.send("Hey Rust. Hello from NextJS");
+      console.log("connected to Rust Server");
+    }
+    return () => {
+      socket.close(1000,"Client wanted to close the socket");
+    }
+  }, [])
   return (
       <div className="flex justify-evenly w-full items-center translate-y-32">
         <div><Player dir={0} detail={{health : 100, character_type : 1, spells : ["fire","ice"]}}/></div>

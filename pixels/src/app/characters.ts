f8aced7e8419928bbb1bf0112c4175bb43acc63f
Aryan <malhotraaryan570@gmail.com>
2024-09-13 08:33:12 +0530

    banner

--- pixels/src/app/characters.ts
+++ pixels/src/app/characters.ts
@@ -11,4 +11,12 @@ export const character_array: ch_array[] = [{
     character: 2,
     character_name: "Genos",
     description: "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+},{
+    character : 3,
+    character_name: "Sonic",
+    description : "lor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco "
 }];
+let i = character_array.length;
+export function get_random() : ch_array{
+    return character_array[Math.floor(Math.random() * i)];
+}
--- pixels/src/app/components/Main.tsx
+++ pixels/src/app/components/Main.tsx
@@ -1,18 +1,32 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { ch_array, get_random } from '../characters'
 const Main = () => {
+  const [character, setcharacter] = useState<ch_array>(get_random());
+  useEffect(() => {
+    const switch_int = setInterval(() => {
+      setcharacter(get_random());
+    }, 7000);
+    return () => {
+      clearInterval(switch_int);
+    }
+  }, [])
+  // on click function for the button
+  // which would call the rust to find a match -- need to write backend code for this
+  // and maybe convert these gifs into some supported format images
   return (
     <div className='flex justify-around items-center h-screen'>
       <div className='w-1/3 h-1/3 bg-orange-500 rounded-lg border-2 flex justify-center items-center'>
         <div className='grid grid-rows-3 grid-cols-5 m-1 border-2 rounded-lg items-center h-[97%]'>
           <div className='flex justify-center items-center col-span-2 row-span-3 border-r-2 h-full'>
-            <Image src={"/3.gif"} height={100} width={100} alt='character'/>
+            <Image src={`/${character.character}.gif`} height={100} width={100} alt='character'/>
           </div>
           <div className='flex justify-center items-center col-start-3 col-span-3 row-span-1 border-b-2 h-full'>
-            Kazuha
+            {character.character_name}
           </div>
-          <div className='flex justify-center items-center col-start-3 col-span-3 row-start-2 row-span-2 h-full px-2'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quisquam vero illum quidem iusto, voluptas excepturi nam iure odit rem repudiandae?
+          <div className='flex justify-center items-center col-start-3 col-span-3 row-start-2 row-span-2 h-full px-2 overflow-hidden'>
+            {character.description}
           </div>
         </div>
       </div>

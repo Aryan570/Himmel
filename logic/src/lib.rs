d05fc812f0502e21c2277ea6b371da603ea672ca
Aryan <malhotraaryan570@gmail.com>
2025-01-20 17:27:10 +0530

    still_stuck_with_matchmaking

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -1,30 +1,40 @@
-use std::{collections::VecDeque, sync::Arc};
-use async_std::{net::{TcpListener, TcpStream, ToSocketAddrs}, stream::StreamExt, sync::RwLock, task::spawn};
-use async_tungstenite::{accept_hdr_async, tungstenite::{handshake::{client::Request, server::Response}, Message, Result}, WebSocketStream};
-// type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-type WebSocket = Arc<RwLock<WebSocketStream<TcpStream>>>;
+use std::collections::VecDeque;
+use async_std::{net::{TcpListener, ToSocketAddrs}, stream::StreamExt, sync::Mutex, task::spawn};
+use async_tungstenite::{accept_hdr_async, tungstenite::{handshake::{client::Request, server::Response}, Message, Result}};
+struct MatchMaking {
+    q : Mutex<VecDeque<String>>
+}
+impl MatchMaking {
+    fn new() -> Self {
+        MatchMaking { q : Mutex::new(VecDeque::new()) }
+    }
+    async fn add(&self, player_id : String){
+        let mut queue = self.q.lock().await;
+        queue.push_back(player_id);
+    }
+    async fn match_players(&self) -> Option<(String,String)>{
+        let mut queue = self.q.lock().await;
+        match queue.len() > 1{
+            true => {
+                let p1 = queue.pop_front().unwrap();
+                let p2 = queue.pop_front().unwrap();
+                Some((p1,p2))
+            }
+            _ => None
+        }
+    }
+}
 pub async fn server(addr : impl ToSocketAddrs) -> Result<()>{
     let listener = TcpListener::bind(addr).await?;
     let mut incoming = listener.incoming();
-    let q: Arc<RwLock<VecDeque<WebSocket>>> = Arc::new(RwLock::new(VecDeque::new()));
     while let Some(stream) = incoming.next().await {
         let stream = stream?;
-        let q = q.clone();
         spawn( async move {
-            let callback = |req : &Request, res : Response| {
-                println!("Request from the client");
-                println!("Path of the request is : {}", req.uri().path());
-                for (ref header, _) in req.headers(){
-                    println!("* {header}");
-                }
+            let callback = |_req : &Request, res : Response| {
                 Ok(res)
             };
-            let websocket = Arc::new(RwLock::new(accept_hdr_async(stream, callback).await.expect("error in msg")));
-            {
-                let mut lock = q.write_arc().await;
-                lock.push_back(websocket.clone());
-            }
-            while let Some(Ok(message)) = websocket.write_arc().await.next().await {
+            let mut websocket = accept_hdr_async(stream, callback).await.expect("error in msg");
+            while let Some(Ok(message)) = websocket.next().await {
                 match message {
                     Message::Text(text) => println!("Text message received: {}",text),
                     Message::Binary(bin) => println!("Binary message received: {:?}",bin),
@@ -32,11 +42,6 @@ pub async fn server(addr : impl ToSocketAddrs) -> Result<()>{
                     _ => {},
                 }
             }
-            let mut lock = q.write_arc().await;
-            if let Some(x) = lock.iter().position(|ws| Arc::ptr_eq(ws, &websocket)){
-                lock.remove(x);
-            }
-            println!("{}",lock.len());
         });
     }
     Ok(())
--- logic/src/main.rs
+++ logic/src/main.rs
@@ -1,6 +1,7 @@
 use async_std::task::block_on;
 use logic::server;
 fn main() {
+    // use the port 8000 as the server
     let connect = server("127.0.0.1:8000");
     let _connect = block_on(connect);
 }

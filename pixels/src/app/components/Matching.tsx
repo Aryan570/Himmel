d6223ad97ddf9d66b01b573792f4815eb4fe93cc
Aryan <malhotraaryan570@gmail.com>
2025-02-07 14:52:42 +0530

    front_end...

--- pixels/src/app/components/Matching.tsx
+++ pixels/src/app/components/Matching.tsx
@@ -17,7 +17,7 @@ export type Move = {
     move_type: number,
     locked: number
 }
-const Matching = (props : {char : string}) => {
+const Matching = (props: { char: string }) => {
     const [sock, setsock] = useState<WebSocket | undefined>(undefined);
     const [players, setplayers] = useState<Move>(
         {
@@ -36,7 +36,7 @@ const Matching = (props : {char : string}) => {
     );
     const [found, setfound] = useState(false);
     function to_rust(e: BaseSyntheticEvent) {
-        let val = e.target.value;
+        let val = e.currentTarget.value;
         let tmp = players;
         tmp.move_type = val;
         // set attacker as well
@@ -72,40 +72,42 @@ const Matching = (props : {char : string}) => {
         // |      |      |
         // |------|------|
         return (
-            <div className='flex'>
-                <div className='flex flex-col h-full w-1/2'>
-                    <div className='h-1/6 w-3/4'>
-                        <p>{players.charac_p1}</p>
-                        <progress value={players.h1}></progress>
-                    </div>
-                    <div className='flex h-5/6'>
-                        <div className='basis-1/6'>
-                            <button value={0} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 0)) && !players.attacker ? true : false}>FA</button>
-                            <button value={1} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 1)) && !players.attacker ? true : false}>AB</button>
-                            <button value={2} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 2)) && !players.attacker ? true : false}>AD</button>
-                            <button value={3} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 3)) && !players.attacker ? true : false}>AR</button>
-                            <button value={4} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 4)) && !players.attacker ? true : false}>NN</button>
+            <div className='flex justify-center items-center h-screen'>
+                <div className='flex justify-center items-center h-2/3 w-2/3 bg-gray-200 rounded-2xl'>
+                    <div className='flex flex-col h-full w-1/2 ml-3'>
+                        <div className='h-1/6 w-3/4'>
+                            <p className='text-orange-600'>{players.charac_p1}</p>
+                            <progress value={players.h1}></progress>
                         </div>
-                        <div className='flex justify-center items-center basis-5/6'>
-                            <Image src={`/${players.charac_p1}.gif`} className='brightness-75' alt='character_1' height={200} width={200} />
+                        <div className='flex h-5/6'>
+                            <div className='flex flex-col basis-1/6'>
+                                <button value={0} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 0)) && !players.attacker ? true : false}>FA</button>
+                                <button value={1} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 1)) && !players.attacker ? true : false}>AB</button>
+                                <button value={2} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 2)) && !players.attacker ? true : false}>AD</button>
+                                <button value={3} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 3)) && !players.attacker ? true : false}>AR</button>
+                                <button value={4} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 4)) && !players.attacker ? true : false}>NN</button>
+                            </div>
+                            <div className='flex justify-center items-center basis-5/6'>
+                                <Image src={`/${players.charac_p1}.gif`} className='brightness-75' alt='character_1' height={100} width={100} />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='flex flex-col h-full w-1/2'>
-                    <div className='h-1/6 w-3/4'>
-                        <p>{players.charac_p2}</p>
-                        <progress value={players.h2}></progress>
-                    </div>
-                    <div className='flex h-5/6'>
-                        <div className='flex justify-center items-center basis-5/6'>
-                            <Image className='transform -scale-x-100 brightness-75' src={`/${players.charac_p2}.gif`} alt='character_1' height={200} width={200} />
+                    <div className='flex flex-col h-full w-1/2 mr-3'>
+                        <div className='h-1/6 text-right'>
+                            <p className='text-orange-600'>{players.charac_p2}</p>
+                            <progress value={players.h2}></progress>
                         </div>
-                        <div className='basis-1/6'>
-                            <button value={0} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 0)) && players.attacker ? true : false}>FA</button>
-                            <button value={1} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 1)) && players.attacker ? true : false}>AB</button>
-                            <button value={2} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 2)) && players.attacker ? true : false}>AD</button>
-                            <button value={3} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 3)) && players.attacker ? true : false}>AR</button>
-                            <button value={4} onClick={to_rust} className='text-slate-50 bg-slate-500' disabled={(1 & (players.locked << 4)) && players.attacker ? true : false}>NN</button>
+                        <div className='flex h-5/6'>
+                            <div className='flex justify-center items-center basis-5/6'>
+                                <Image className='transform -scale-x-100 brightness-75' src={`/${players.charac_p2}.gif`} alt='character_1' height={100} width={100} />
+                            </div>
+                            <div className='flex flex-col basis-1/6'>
+                                <button value={0} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 0)) && players.attacker ? true : false}>FA</button>
+                                <button value={1} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 1)) && players.attacker ? true : false}>AB</button>
+                                <button value={2} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 2)) && players.attacker ? true : false}>AD</button>
+                                <button value={3} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 3)) && players.attacker ? true : false}>AR</button>
+                                <button value={4} onClick={to_rust} className='text-slate-50 my-1 bg-slate-500' disabled={(1 & (players.locked << 4)) && players.attacker ? true : false}>NN</button>
+                            </div>
                         </div>
                     </div>
                 </div>
